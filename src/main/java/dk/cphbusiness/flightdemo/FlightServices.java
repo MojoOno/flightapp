552c8eaa1d564e869a72a13120895ed14b01e029
MojoOno <Ffranck0406@gmail.com>
2025-01-29 12:40:35 +0100

    Created method calculating planes in air from a given airline at a given time

--- src/main/java/dk/cphbusiness/flightdemo/FlightServices.java
+++ src/main/java/dk/cphbusiness/flightdemo/FlightServices.java
@@ -4,6 +4,7 @@ import dk.cphbusiness.flightdemo.dtos.FlightDTO;
 import dk.cphbusiness.flightdemo.dtos.FlightInfoDTO;
 
 import java.io.IOException;
+import java.time.LocalDateTime;
 import java.util.List;
 
 import static dk.cphbusiness.flightdemo.FlightReader.getFlightInfoDetails;
@@ -12,12 +13,14 @@ import static dk.cphbusiness.flightdemo.FlightReader.getFlightsFromFile;
 public class FlightServices
 {
     public static void main(String[] args) throws IOException
-    {System.out.println(countDeparturesPerAirline("Royal Jordanian"));
-
-
+    {
+        System.out.println(countDeparturesPerAirline("Turkish Airlines"));
+        System.out.println(planesInAir("Turkish Airlines",
+                LocalDateTime.of(2024, 8, 15, 02, 0, 0)));
     }
 
-    public static long countDeparturesPerAirline(String airlineName) throws IOException {
+    public static long countDeparturesPerAirline(String airlineName) throws IOException
+    {
         List<FlightDTO> flightList = getFlightsFromFile("flights.json");
         List<FlightInfoDTO> flightInfoDTOList = getFlightInfoDetails(flightList);
 
@@ -29,5 +32,19 @@ public class FlightServices
         return result;
     }
 
+    public static long planesInAir(String airlineName, LocalDateTime time) throws IOException
+    {
+        List<FlightDTO> flightList = getFlightsFromFile("flights.json");
+        List<FlightInfoDTO> flightInfoDTOList = getFlightInfoDetails(flightList);
+
+        long result = flightInfoDTOList.stream()
+                .filter(flight -> flight.getAirline() != null)
+                .filter(flight -> flight.getAirline().equals(airlineName))
+                .filter(flight -> flight.getDeparture().isBefore(time))
+                .filter(flight -> flight.getArrival().isAfter(time))
+                .count();
+
+        return result;
+    }
 
 }
